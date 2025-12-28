dd5f82496709a53f0ba8f70b95f790df3238cd68
Jose Zarabanda <jlzarabandad@gmail.com>
2025-12-28 04:54:45 -0500

    [deploy] Change default track to alpha (prueba cerrada) v1.3.1

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.zarabandajose.offlineaudionotes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild { cmake { cppFlags += "-std=c++17" } }
